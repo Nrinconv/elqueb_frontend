7c0a5ff04c0b9ee4e2ddd1b940b8650c58ea074c
Nrinconv <nrinconv@unal.edu.co>
2023-11-14 19:48:10 -0500

    init

---
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
+import { AcmeLogo } from "./AcmeLogo.jsx";
+
+function NavigationBar() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    {
+      name: "Inicio",
+      linkTo: "home",
+    },
+    {
+      name: "Ofertas",
+      linkTo: "offers",
+    },
+    {
+      name: "Estadísticas",
+      linkTo: "stats",
+    },
+  ];
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <AcmeLogo />
+          <p className="font-bold text-inherit">ELQUEB</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        {menuItems.map((item, index) => (
+          <NavbarItem key={`${item}-${index}`}>
+            <Link color="foreground" href={item.linkTo}>
+              {item.name}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="signin">Inicio de sesión</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="signup" variant="flat">
+            Regitrarse
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2
+                  ? "primary"
+                  : index === menuItems.length - 1
+                  ? "danger"
+                  : "foreground"
+              }
+              className="w-full"
+              href={item.linkTo}
+              size="lg"
+            >
+              {item.name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
