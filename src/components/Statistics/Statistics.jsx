e3391b8eaa1942f62698695a5a73e7690c540f51
Nrinconv <nrinconv@unal.edu.co>
2023-11-22 12:48:30 -0500

    statistics home fixed

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,30 +1,61 @@
-import './StatisticsComponent.css';
+import "./StatisticsComponent.css";
 import { Divider } from "@nextui-org/react";
-import { GiStrong } from "react-icons/gi";
+import { TbSquareRoundedNumber1Filled } from "react-icons/tb";
+
+import { FaHandsHelping, FaLightbulb } from "react-icons/fa";
+import { MdStarRate } from "react-icons/md";
+import { LiaPeopleCarrySolid } from "react-icons/lia";
 
 function Statistics() {
-    const stats = [
-      { icon: <GiStrong />, title: 'Emprededores', description: 'Hemos apoyado alrededor 10M' },
-      { icon: <GiStrong />, title: 'Reviews', description: '4.5 average rating' },
-      { icon: <GiStrong />, title: 'Favorites', description: 'Over 1,000 favorites' },
-      { icon: <GiStrong />, title: 'Global Reach', description: 'Users from over 100 countries' },
-    ];
-  
-    return (
-        <div>
-          <Divider/>
-          <h2 className="statistics-title">Nuestras Cifras</h2>
-          <div className="statistics-container">
-            {stats.map((stat, index) => (
-              <div key={index} className="statistic">
-                <div>{stat.icon}</div>
-                <h3>{stat.title}</h3>
-                <p>{stat.description}</p>
-              </div>
-            ))}
+  const stats = [
+    {
+      icon: <FaHandsHelping />,
+      title: "Emprededores",
+      description:
+        "Hemos brindado apoyo a más de 8000 actores de la economía popular",
+    },
+    {
+      icon: <MdStarRate />,
+      title: "Opinión",
+      description:
+        "Nuestra calificación promedio de 4.5 refleja la satisfacción constante de nuestros usuarios.",
+    },
+    {
+      icon: <TbSquareRoundedNumber1Filled />,
+      title: "Posición",
+      description:
+        "Somos los mejores conectando oportunidades con emprendedores",
+    },
+    {
+      icon: <FaLightbulb />,
+      title: "Ideas Implementadas",
+      description:
+        "Hemos convertido más de 50 ideas innovadoras en realidades exitosas",
+    },
+    {
+      icon: <LiaPeopleCarrySolid />,
+      title: "Equipo Comprometido",
+      description:
+        "Valoramos sus opiniones, ofreciendo una experiencia que mejora continuamente",
+    },
+  ];
+
+  return (
+    <div>
+      <h2 className="statistics-title">Nuestras Cifras</h2>
+      <div className="statistics-container">
+        {stats.map((stat, index) => (
+          <div key={index} className="statistic">
+            <div style={{ fontSize: "50px", color: "#009ee2" }}>
+              {stat.icon}
+            </div>
+            <h3>{stat.title}</h3>
+            <p>{stat.description}</p>
           </div>
-        </div>
-      );
-  }
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default Statistics;
