ed2942b7de19f1d06785e6731045bc2c0fbeee0e
Julian ro <davidro2121@gmail.com>
2023-11-22 13:23:27 -0500

    side bar

--- src/components/AccessibilityBar/AccessibilityBar.jsx
+++ src/components/AccessibilityBar/AccessibilityBar.jsx
@@ -1,9 +1,12 @@
 import { Button } from '@mui/base';
 import { useState } from 'react';
+import './AccessibilityBar.css';
 
 const AccessibilityBar = () => {
 
     const [invertColors, setInvertColors] = useState(false);
+    const [fontSize, setFontSize] = useState(100);
+
     const toggleColors = () => {
         setInvertColors(!invertColors);
         if (!invertColors) {
@@ -14,13 +17,25 @@ const AccessibilityBar = () => {
         document.body.style.backgroundColor = 'white';
         }
     };
+    const increaseFontSize = () => {
+        setFontSize(prevFontSize => prevFontSize + 10);
+        document.documentElement.style.fontSize = `${fontSize}%`;
+      };
+    const decreaseFontSize = () => {
+        setFontSize(prevFontSize => Math.max(prevFontSize - 10, 50));
+        document.documentElement.style.fontSize = `${fontSize}%`;
+      };
     return (
         <div className="accessibility-bar" >
             <Button onClick={toggleColors}>
                 Toggle Colors
             </Button>
-            <button>Button 2</button>
-            <button>Button 3</button>
+            <Button onClick={increaseFontSize}>
+                Increase Font Size
+            </Button>
+            <Button onClick={decreaseFontSize}>
+                Decrease Font Size
+            </Button>
         </div>
     );
 };
