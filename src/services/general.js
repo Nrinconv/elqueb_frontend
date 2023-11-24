c120c3784b07047632662a93adee7c317c1e5d00
Nrinconv <nrinconv@unal.edu.co>
2023-11-24 07:15:04 -0500

    fixed 1

--- src/services/general.js
+++ src/services/general.js
@@ -1,21 +1,20 @@
 import axios from "axios";
 
-const apiUrl = " localhost:8000/";
+const apiUrl = " localhost:8000";
 
 export const busquedaInfo = async (busqueda) => {
-  const response = await axios.post(`${apiUrl}/ofertas/${busqueda}`, {
-    /* params: {
-      start_date,
-      end_date,
+  const response = await axios.post(`${apiUrl}/ofertas/semanticSearch`, {
+    params: {
+      search_term: busqueda,
     },
-    headers: "getHeaders()", */
+    /* headers: "getHeaders()", */
   });
 
   return response;
 };
 
 export const recommendedInfo = async (id) => {
-  const response = await axios.get(`${apiUrl}-ofertas/recommendOfertas/${id}`, {
+  const response = await axios.get(`${apiUrl}/ofertas/recommendOfertas/${id}`, {
     /* params: {
       start_date,
       end_date,
