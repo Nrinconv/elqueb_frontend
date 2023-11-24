cf2a7e68dc3f0a33e1bdafbec47d76afafd16ac6
NickGalindo <ngalindoc@unal.edu.co>
2023-11-24 07:15:31 -0500

    fixed and a

--- src/pages/addOffer/addOffer.jsx
+++ src/pages/addOffer/addOffer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Formik, Field, Form } from 'formik';
-import './AddOffer.css';
+import './addOffer.css';
 
 const AddOffer = () => {
   const handleSubmit = (values) => {
